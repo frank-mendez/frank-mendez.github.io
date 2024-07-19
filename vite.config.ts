8df736c31539a3b172918cad652174c61baa2505
Frank Mendez <frankmendezresources@gmail.com>
2024-07-19 12:31:11 +0800

    fix: deploy page

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,5 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: path.resolve(__dirname, '')
-  }
+  base: '/frank-mendez.github.io/'
 })
